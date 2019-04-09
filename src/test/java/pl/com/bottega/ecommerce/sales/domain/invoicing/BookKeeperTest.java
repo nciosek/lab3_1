72d4e2455c0b7349cd35cb7d63746f9c4cb0c032
Natalia Ciosek <n_ciosek@wp.pl>
2019-04-09 18:24:01 +0200

    isolation - dodanie metody setup

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,11 +1,14 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -18,20 +21,26 @@ import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
 public class BookKeeperTest {
 
-    InvoiceFactory invoiceFactory = new InvoiceFactory();
-    ClientData clientData = new ClientData(Id.generate(), "nazwisko");
-    InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+    InvoiceFactory invoiceFactory;
+    ClientData clientData;
+    InvoiceRequest invoiceRequest;
+    BookKeeper bookKeeper;
+
+    @Before public void setup(){
+        invoiceFactory = new InvoiceFactory();
+        clientData = new ClientData(Id.generate(), "Ciosek");
+        invoiceRequest = new InvoiceRequest(clientData);
+        bookKeeper = new BookKeeper(invoiceFactory);
+    }
 
     @Mock TaxPolicy taxPolicy;
 
-    //test stanu
     @Test public void emptyInvoice(){
         BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
         assertThat(invoice.getItems(), empty());
     }
 
-    //test zachowania
     @Test public void emptyInvoiceNoCallTaxPolicy(){
         BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
