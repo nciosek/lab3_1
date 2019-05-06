61bb8b4dad90aa9b2f4caa2c44543a86cb37b4c5
Natalia Ciosek <n_ciosek@wp.pl>
2019-05-06 15:30:00 +0200

    builder - dodanie klasy AddProductCommandHandlerBuilder

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerBuilder.java
@@ -0,0 +1,45 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
+public class AddProductCommandHandlerBuilder {
+    private ReservationRepository reservationRepository;
+    private ProductRepository productRepository;
+    private SuggestionService suggestionService;
+    private ClientRepository clientRepository;
+    private SystemContext systemContext;
+
+    public AddProductCommandHandlerBuilder setReservationRepository(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+        return this;
+    }
+
+    public AddProductCommandHandlerBuilder setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+        return this;
+    }
+
+    public AddProductCommandHandlerBuilder setSuggestionService(SuggestionService suggestionService) {
+        this.suggestionService = suggestionService;
+        return this;
+    }
+
+    public AddProductCommandHandlerBuilder setClientRepository(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+        return this;
+    }
+
+    public AddProductCommandHandlerBuilder setSystemContext(SystemContext systemContext) {
+        this.systemContext = systemContext;
+        return this;
+    }
+
+    public AddProductCommandHandler createAddProductCommandHandler() {
+        return new AddProductCommandHandler(reservationRepository, productRepository, suggestionService, clientRepository, systemContext);
+    }
+
+}
